646b894e4e34554c490d271b52da7f64e2940efe
Vincent James Paz <pavi@erni.ph>
2020-09-11 18:47:30 +0800

    with driving

---
+++ Assets/Driving.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driving : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.forward);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.up);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up);
+        }
+    }
+}
